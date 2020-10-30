aeaa6997fc455754c02cd6a3f6b65b2535ab5f11
Wingkou Loeng <wingkou@outlook.com>
2020-10-31 01:12:39 +0800

    v1.1

--- TDEDecrypt/main.cpp
+++ TDEDecrypt/main.cpp
@@ -9,7 +9,7 @@
 const char* const a = "599Cc51887A8cb0C20F9CdE34cf9e0A535E5cAd1C26c7b562596ACC207Ae9A0bfB3E3161f31af5bEf1c2f064b3628174D83BF6E0739D9D6918fD9C2Eba02D5aD\0";
 const char* const b = "0C3b676fe8d7188Cde022F71632830F36b98b181aD48Fed160006eA59\0";
 
-auto input = "D:\\Steam\\steamapps\\common\\Teardown\\\data\\music\\menu.ogg.tde";
+auto input = "D:\\Steam\\steamapps\\common\\Teardown\\data\\music\\menu.ogg.tde";
 auto output = "D:\\Project\\TD\\test\\menu.ogg";
 //auto input = "D:\\Steam\\steamapps\\common\\Teardown\\data\\snd\\alarm-background.ogg.tde";
 //auto output = "D:\\Project\\TD\\test\\alarm-background.ogg";
