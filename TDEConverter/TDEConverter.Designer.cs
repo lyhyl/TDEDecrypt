7707f2233628d42759f3c8ed3f55690b8bd1c883
Wingkou Loeng <wingkou@outlook.com>
2020-11-03 20:29:52 +0800

    GUI

---
+++ TDEConverter/TDEConverter.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TDEConverter
+{
+    partial class TDEConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(209, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 217);
+            this.listBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 217);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 44);
+            this.panel1.TabIndex = 3;
+            // 
+            // TDEConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "TDEConverter";
+            this.Text = "TDEConverter";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TDEConverter/TDEConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TDEConverter
+{
+    public partial class TDEConverter : Form
+    {
+        public TDEConverter()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> fileNames = new List<string>();
+        private const string a = "599Cc51887A8cb0C20F9CdE34cf9e0A535E5cAd1C26c7b562596ACC207Ae9A0bfB3E3161f31af5bEf1c2f064b3628174D83BF6E0739D9D6918fD9C2Eba02D5aD";
+        private const string b = "0C3b676fe8d7188Cde022F71632830F36b98b181aD48Fed160006eA59";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileNames.AddRange(dialog.FileNames);
+                    listBox1.Items.AddRange(dialog.FileNames.Select(n => n.EndsWith(".tde") ? n + " (decrypt)" : n + " (encrypt)").ToArray());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> errors = new List<string>();
+            foreach (var fileName in fileNames)
+            {
+                try
+                {
+                    Convert(fileName);
+                }
+                catch (Exception ex)
+                {
+                    errors.Add(ex.Message);
+                }
+            }
+            if (errors.Count != 0)
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, errors), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("All Done", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            listBox1.Items.Clear();
+        }
+        private void Convert(string fileName)
+        {
+            string outpath = fileName.EndsWith(".tde") ? fileName.Substring(0, fileName.Length - 4) : fileName + ".tde";
+            using (FileStream fin =new FileStream(fileName,FileMode.Open), fout =new FileStream(outpath,FileMode.Create))
+            {
+                int dat, i = 0;
+                while ((dat = fin.ReadByte()) != -1)
+                {
+                    var d = (byte)dat ^ (byte)a[i % a.Length] ^ (byte)b[i % b.Length];
+                    fout.WriteByte((byte)d);
+                    i++;
+                }
+            }
+        }
+    }
+}
